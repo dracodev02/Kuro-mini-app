9f39cd825fe2bd7790df73632c7efa6cf3f016f9
giabaoitwork <139120902+dracodev02@users.noreply.github.com>
2025-05-22 00:01:39 +0700

    fix rewrite

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,24 @@
 import type { NextConfig } from "next";
 
+const API_HOST = process.env.NEXT_PUBLIC_API_HOST;
+
+if (!API_HOST) {
+  throw new Error("Missing NEXT_PUBLIC_API_HOST in environment variables");
+}
+
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: process.env.NEXT_PUBLIC_API_HOST + "/:path*",
+        destination: `${API_HOST}/:path*`,
       },
     ];
   },
   webpack: config => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding')
-    return config
-  }
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
 };
 
 export default nextConfig;
