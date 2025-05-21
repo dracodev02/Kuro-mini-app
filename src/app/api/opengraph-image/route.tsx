4aed4ab26c5d959673e2a37113fb24c77a06ac4b
giabaoitwork <139120902+dracodev02@users.noreply.github.com>
2025-05-22 02:24:25 +0700

    fix opengraph-image

--- src/app/api/opengraph-image/route.tsx
+++ src/app/api/opengraph-image/route.tsx
@@ -1,21 +1,24 @@
-import Image from "next/image";
 import { ImageResponse } from "next/og";
 import { NextRequest } from "next/server";
+import { getNeynarUser } from "~/lib/neynar";
 
 export const dynamic = "force-dynamic";
 
 export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const fid = searchParams.get("fid");
+
+  const user = fid ? await getNeynarUser(Number(fid)) : null;
+
   return new ImageResponse(
     (
-      <Image
-        src={"/immages/kuro-logo.svg"}
-        alt="Kuro Logo"
-        width={800}
-        height={800}
-      />
+      <div tw="flex h-full w-full flex-col justify-center items-center relative bg-purple-600">
+        <h1 tw="text-8xl text-white">Kuro Game</h1>
+        <p tw="text-5xl mt-4 text-white opacity-80">Powered by Fukunad.xyz</p>
+      </div>
     ),
     {
-      width: 800,
+      width: 1200,
       height: 800,
     }
   );
